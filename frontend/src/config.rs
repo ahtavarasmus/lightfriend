3d342e92e0ff62038451cef4310e9731a23a8e32
ahtavarasmus <rasmus@ahtava.com>
2025-06-01 21:29:04 -0700

    x

--- frontend/src/config.rs
+++ frontend/src/config.rs
@@ -3,4 +3,8 @@ pub fn get_backend_url() -> &'static str {
     "http://localhost:3000"  // Development URL when running locally
 }
 
+#[cfg(not(debug_assertions))]
+pub fn get_backend_url() -> &'static str {
+    ""  // Production URL
+}
 
